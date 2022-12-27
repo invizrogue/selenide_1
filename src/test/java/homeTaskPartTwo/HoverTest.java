fd0f3d33abe3076c0ed4e393b7e3a24214b67503
dmikhaylov <d.y.mikhaylov@ya.ru>
2022-12-27 10:10:50 +0400

    added url-check

--- src/test/java/homeTaskPartTwo/HoverTest.java
+++ src/test/java/homeTaskPartTwo/HoverTest.java
@@ -1,9 +1,11 @@
 package homeTaskPartTwo;
 
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HoverTest {
 
@@ -16,5 +18,7 @@ public class HoverTest {
         $("a.HeaderMenu-dropdown-link[href$='enterprise']").shouldBe(visible).click();
         // проверить, что есть заголовок - Build like the best
         $(withTagAndText("h1", "Build like the best")).should(exist);
+        // проверить, что url ожидаемый
+        assertEquals("https://github.com/enterprise", WebDriverRunner.url());
     }
 }
